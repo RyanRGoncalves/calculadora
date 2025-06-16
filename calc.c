9825aae8f3e26edf88c12b5e15b0144e6add1087
RyanRG <ryan.goncalves19@etec.sp.gov.br>
2025-06-16 17:22:49 -0300

    Introdução basica

--- calc.c
+++ calc.c
@@ -2,8 +2,27 @@
 #include <stdlib.h>
 
 void main() {
+	// Introdução
 	int escolha;
 	printf("Bem vindo a calculadora, digite o número da operação para escolher-lá:\n1. Adição\n2. Subtração\n3. Multiplicação\n4. Divisão\n\n");
 	scanf("%d",&escolha);
-	printf("Voçe escolheu a opção %d\n",escolha);
+	
+	//Calculos
+	switch (escolha) {
+		case 1:
+			printf("Adição");
+			break;
+		case 2:
+			printf("Subtração");
+			break;
+		case 3:
+			printf("Multiplicação");
+			break;
+		case 4: 
+			printf("Divisão");
+			break;
+		default:
+			printf("Fora do nosso alcance.");
+			break;
+	}
 }
