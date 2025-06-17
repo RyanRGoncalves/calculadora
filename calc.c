8a539c5ebb51236f64a72f20f5040ced702ab865
RyanRG <ryan.goncalves19@etec.sp.gov.br>
2025-06-17 17:38:29 -0300

    Mais testes

--- calc.c
+++ calc.c
@@ -8,13 +8,15 @@ void main() {
 	char escolhachar;
 	float num[2];
 	float resultado;
-
+	
+	scanf("%d",&escolhaint);
 	scanf("%c",&escolhachar);
+	printf("%c",escolhachar);
+	printf("%d",escolhaint);
 
 	printf("Bem vindo a calculadora, digite o número da operação para escolher-lá:");
 	do {
 		escolhaint = intqna("\n1. Adição\n2.Subtração\n3.Multiplicação\n4.Divisão\n\n");
-
 		//Calculos
 		switch (escolhaint) {
 			case 1:
@@ -34,10 +36,11 @@ void main() {
 				printf("Divisão");
 				break;
 			default:
-				printf("Fora do nosso alcance.");
+				printf("Fora do nosso alcance.\n");
 				break;
 		}
-		escolhachar = charqna("Deseja continuar a usar a calculadora?\n Pressione \"q\" para sair, \"p\" para continuar.");
+		//escolhachar = charqna("Deseja continuar a usar a calculadora?\nPressione q para sair, p para continuar.");
+		//scanf("%c",&escolhachar);
 		if(escolhachar == 'q' || escolhachar == 'Q') {
 			printf("Então escolha novamente qual operação deseja utilizar.");
 		}
